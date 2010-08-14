0a49aa808f86b9783abd8a8c8eb5860790d1baec
Kip <kip@krispowell.net>
2010-08-14 23:11:39 +0100

    spec passing, now needs cleanup

--- src/emma-scrape.rb
+++ src/emma-scrape.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 class EmmaScraper
 
   def parse_package( package_row )
+    #p package_row
     package_name = package_row.xpath( './/a' ).first.content
     method_cover = package_row.xpath( './/td' )[2].content
     covered, uncovered = method_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_i )
@@ -14,6 +15,7 @@ class EmmaScraper
   
   def go
     doc = Nokogiri::HTML(open('http://emma.sourceforge.net/coverage_sample_a/index.html'))  
-    doc.xpath('//tr[@class="o"]').map{|row| parse_package( row ) }
+    # :( doesn't seem to be a good way to identify our table 'cleanly', this is brittle
+    doc.xpath('//table[4]/tr/td/..').map{|row| parse_package( row ) }
   end
 end
--- src/scraper_spec.rb
+++ src/scraper_spec.rb
@@ -3,7 +3,9 @@ require 'emma-scrape'
 describe EmmaScraper, "#check final piece" do
   it "returns package name" do
     scraper = EmmaScraper.new
-    package, uncovered, covered = scraper.go.last
+    results = scraper.go
+    results.should have(26).items
+    package, uncovered, covered = results.last
     package.should == "org.apache.velocity.exception"
   end
 end
