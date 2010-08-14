08a714292efd671d8d96e238e154c806d86d2de3
Kip <kip@krispowell.net>
2010-08-14 22:51:16 +0100

    Structure in place, but test still failing

--- src/emma-scrape.rb
+++ src/emma-scrape.rb
@@ -1,17 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 
-def parse_package( package_row )
-  package_name = package_row.xpath( './/a' ).first.content
-  method_cover = package_row.xpath( './/td' )[2].content
-  covered, uncovered = method_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_i )
-  printf "%s : %s\n", package_name, method_cover
-  printf "%s , %s\n%s\n", covered, uncovered, covered.class
-  return package_name, covered, uncovered   
-end
+class EmmaScraper
 
-doc = Nokogiri::HTML(open('http://emma.sourceforge.net/coverage_sample_a/index.html'))
-
-doc.xpath('//tr[@class="o"]').each do |row|
-  parse_package( row )
+  def parse_package( package_row )
+    package_name = package_row.xpath( './/a' ).first.content
+    method_cover = package_row.xpath( './/td' )[2].content
+    covered, uncovered = method_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_i )
+    #printf "%s : %s\n", package_name, method_cover
+    #printf "%s , %s\n%s\n", covered, uncovered, covered.class
+    return package_name, covered, uncovered   
+  end
+  
+  def go
+    doc = Nokogiri::HTML(open('http://emma.sourceforge.net/coverage_sample_a/index.html'))  
+    doc.xpath('//tr[@class="o"]').map{|row| parse_package( row ) }
+  end
 end
