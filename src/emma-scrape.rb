ffb350602e5acd012cdb9b4c49618c51b343f056
Kip <kip@krispowell.net>
2010-08-14 23:15:48 +0100

    mucking w branch

--- src/emma-scrape.rb
+++ src/emma-scrape.rb
@@ -4,12 +4,9 @@ require 'open-uri'
 class EmmaScraper
 
   def parse_package( package_row )
-    #p package_row
     package_name = package_row.xpath( './/a' ).first.content
     method_cover = package_row.xpath( './/td' )[2].content
     covered, uncovered = method_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_i )
-    #printf "%s : %s\n", package_name, method_cover
-    #printf "%s , %s\n%s\n", covered, uncovered, covered.class
     return package_name, covered, uncovered   
   end
   
