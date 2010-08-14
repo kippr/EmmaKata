efc6af0b50e8ac121c97b2ac247c54a157dcf5fb
Kip <kip@krispowell.net>
2010-08-14 21:31:13 +0100

    Refactor parse into a method

--- src/emma-scrape.rb
+++ src/emma-scrape.rb
@@ -1,12 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
 
+def parse_package( package_row )
+  package_name = package_row.xpath( './/a' ).first.content
+  method_cover = package_row.xpath( './/td' )[2].content
+  covered, uncovered = method_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_i )
+  printf "%s : %s\n", package_name, method_cover
+  printf "%s , %s\n%s\n", covered, uncovered, covered.class   
+end
+
 doc = Nokogiri::HTML(open('http://emma.sourceforge.net/coverage_sample_a/index.html'))
 
 doc.xpath('//tr[@class="o"]').each do |row|
-  package = row.xpath('.//a').first.content
-  method_cover = row.xpath('.//td')[2].content
-  covered, uncovered = method_cover.scan(/\((\d+)\/(\d+)/).first.map(&:to_i)
-  printf "%s : %s\n", package, method_cover
-  printf "%s , %s\n%s\n", covered, uncovered, covered.class
+  parse_package( row )
 end
