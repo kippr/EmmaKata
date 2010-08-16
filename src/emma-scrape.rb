835da61bce20c946c8b3eece87ecffd35a872ab9
Kip <github@krispowell.net>
2010-08-16 23:49:13 +0100

    clean up scraper class ordering, visibility

--- src/emma-scrape.rb
+++ src/emma-scrape.rb
@@ -7,28 +7,13 @@ class EmmaScraper
     @url = url
   end
 
-  def parse_package_info( row )
-    
-    # the td with link has our package name in it
-    package = row.xpath( './td/a' ).first.content
-    
-    # and the 4th td is the block cover column
-    block_cover = row.xpath( './td' )[3].content
-    
-    # regex is hard to read- this is capturing numeric X,Y from "(X/Y)"
-    # result is list of capture groups, coerce first 'set' of captures into floats
-    covered, total = block_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_f )
-    
-    return package, covered, total   
-  end
-  
   def scrape
     doc = Nokogiri::HTML(open(@url))
     # :( doesn't seem to be a good way to identify our table 'cleanly', this is brittle
     # table 4 is our package table, and we want rows that have data cells, not header
     doc.xpath('//table[4]/tr/td/..').map{|row| parse_package_info( row ) }
   end
-  
+
   def roll_up
     results = Hash.new([0,0])
     scrape.each do |package, this_covered, this_total|
@@ -44,6 +29,22 @@ class EmmaScraper
     end
     results
   end
+
+  private
+    def parse_package_info( row )
+
+      # the td with link has our package name in it
+      package = row.xpath( './td/a' ).first.content
+
+      # and the 4th td is the block cover column
+      block_cover = row.xpath( './td' )[3].content
+
+      # regex is hard to read- this is capturing numeric X,Y from "(X/Y)"
+      # result is list of capture groups, coerce first 'set' of captures into floats
+      covered, total = block_cover.scan( /\((\d+)\/(\d+)/ ).first.map( &:to_f )
+
+      [package, covered, total]
+    end
     
 end
 
